ed66e506c6d4c56662a1995597a1a6ed69500bf1
longd <longd@NONG>
2025-02-15 01:41:19 +0700

    Add project files.

---
+++ WpfApp1/Test01/Classtess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace K009
+{
+    [TestFixture]
+    internal class Classtess
+    {
+        [Test]
+        public void TestMethod()
+        {
+            string Name = "Diêm Đăng Long";
+
+            Console.WriteLine(Name);
+        }
+    }
+}
